7e61badfb12d28dbb7327f0d3fca1134f5611217
Yoana Bastiyanova <bastianovajoana@gmail.com>
2026-03-26 16:28:23 +0200

    When Creating User, create Profile. RegisterForm already handles this, but it won't work for admin creation, so adding this as a fallback the create_profile_for_user signal

--- users/signals.py
+++ users/signals.py
@@ -1,4 +1,5 @@
-from django.db.models.signals import pre_save, post_delete
+from django.contrib.auth.models import User, Group
+from django.db.models.signals import pre_save, post_delete, post_save
 from django.dispatch import receiver
 from .models import Profile
 
@@ -21,3 +22,14 @@ def delete_old_profile_picture(sender, instance, **kwargs):
 def delete_profile_picture_on_delete(sender, instance, **kwargs):
     if instance.profile_picture:
         instance.profile_picture.delete(save=False)
+
+
+@receiver(post_save, sender=User)
+def create_profile_for_user(sender, instance, created, **kwargs):
+    """
+    When Creating User, create Profile. RegisterForm already handles this, but it won't work for admin creation, so adding this as a fallback
+    """
+    if created:
+        Profile.objects.get_or_create(user=instance)
+        group = Group.objects.get_or_create(name='User')[0]
+        instance.groups.add(group)
